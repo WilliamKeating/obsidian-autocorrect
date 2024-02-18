6f710166542d174b58894a24babf1434833afcd7
wwwwww6wwwww6 <wjkeats@gmail.com>
2024-02-17 17:22:28 -0800

    Implemented mvp autocorrect functionality

--- main.ts
+++ main.ts
@@ -1,134 +1,184 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Editor,
+	Plugin,
+	MarkdownView,
+	Notice,
+	RequestUrlParam,
+	requestUrl,
+	PluginSettingTab,
+	Setting,
+	App,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	api_key: string;
+	status_notices: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: PluginSettings = {
+	status_notices: true,
+	api_key: "",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoCorrecter extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.onKeyDown = this.onKeyDown.bind(this);
+		this.registerDomEvent(document, "keydown", this.onKeyDown, true);
+		this.addSettingTab(new SettingTab(this.app, this));
+		if (!this.settings.api_key) {
+			new Notice(
+				"Please enter your Together.ai API key in the settings tab to use Obsidian AutoCorrect."
+			);
+		}
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async onKeyDown(event: KeyboardEvent) {
+		if (event.key === "Enter") {
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			// Make sure the user is editing a Markdown file.
+			if (view) {
+				const cursor = view.editor.getCursor();
+				const text = view.editor.getLine(cursor.line);
+				console.log(cursor);
+				console.log(text);
+				let status: Notice | null = null;
+				if (this.settings.status_notices) {
+					status = new Notice(
+						"Correcting spelling on line " +
+							(cursor.line + 1) +
+							"...",
+						0
+					);
+				}
+				const response: any = await this.getLLMResponse(text);
+				console.log(response);
+				if (response.corrected_spelling) {
+					view.editor.setLine(
+						cursor.line,
+						response.corrected_spelling
+					);
+					if (status) {
+						status.hide();
+					}
+				} else {
+					if (status) {
+						status.setMessage(
+							"Error correcting spelling. Make sure API key is correct and Internet is working."
+						);
+					} else {
+						new Notice(
+							"Error correcting spelling. Make sure API key is correct and Internet is working."
+						);
+					}
+				}
+			}
+		}
+	}
+	async getLLMResponse(text: string) {
+		const url = "https://api.together.xyz/v1/chat/completions";
+		const apiKey = this.settings.api_key;
+		const system_content =
+			"You will receive user input containing text with potential spelling errors. Your task is to correct these errors while preserving the original meaning. The original text may contain markdown, which should be maintained. You should ONLY correct spelling errors; grammar and punctuation should remain EXACTLY the same. The output should be a corrected version of the input text. Additionally, the model should leave correct words verbatim, even if they may be offensive, slang, abbreviations, brand names, or other non-standard language intentionally input by the user. This system will be utilized for real-time autocorrection, so refrain from altering a word unless you understand the user's intended meaning.";
+		const user_content = '{"user_text": "' + text + '"}';
+
+		const headers: Record<string, string> = {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${apiKey}`,
+		};
+
+		const data = {
+			model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+			messages: [
+				{ role: "system", content: system_content },
+				{ role: "user", content: user_content },
+			],
+			response_format: {
+				type: "json_object",
+				schema: {
+					type: "object",
+					properties: {
+						corrected_spelling: { type: "string" },
+					},
+					required: ["corrected_spelling"],
+				},
+			},
+			temperature: 0.7,
+			max_tokens: 500,
+		};
+
+		const params: RequestUrlParam = {
+			method: "POST",
+			headers,
+			body: JSON.stringify(data),
+			url: url,
+		};
+		try {
+			const response = await requestUrl(params);
+			const json_response = JSON.parse(
+				response.json.choices[0].message.content
+			);
+			return json_response;
+		} catch (error) {
+			console.error(error);
+			return error;
+		}
+	}
 }
+class SettingTab extends PluginSettingTab {
+	plugin: AutoCorrecter;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AutoCorrecter) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Together.ai API Key")
+			.setDesc(
+				"Create a account at https://www.together.ai/ and enter your API key for the plugin to work."
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your key")
+					.setValue(this.plugin.settings.api_key)
+					.onChange(async (value) => {
+						this.plugin.settings.api_key = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Status Notices")
+			.setDesc("Show status notice popups when correcting spelling.")
+			.addToggle((toggle) => {
+				toggle
+					.setValue(this.plugin.settings.status_notices)
+					.onChange(async (value) => {
+						this.plugin.settings.status_notices = value;
+						await this.plugin.saveSettings();
+					});
+			});
 	}
 }
